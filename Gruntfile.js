e34073481ea73f20cf9f25c0d6cb8eb125fc8f8e
microtroll <mtrolldev@gmail.com>
2015-06-04 23:36:48 +0200

    Add more test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,20 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.initConfig({
     qunit: {
       files: ['test/index.html']
+    },
+    uglify: {
+      js: {
+        files: {
+          'jquery.travelmap.min.js': ['jquery.travelmap.js']
+        }
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.registerTask('test', 'qunit');
+  grunt.registerTask('default', ['uglify', 'qunit']);
 };
--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,31 @@
 QUnit.test('Should display the default map', function(assert) {
   assert.ok($('#map-default').length, 'container element is not visible');
-  //assert.ok($('#map-default .gm-style').length, 'iframe is not visible');
+  assert.ok($('#map-default div').length, 'map is not visible');
   assert.equal($('#map-default').css('height'), '500px', 'default height is not 500px');
   assert.equal($('#map-default').css('width'), '550px', 'default width is not 550px');
 });
 
 QUnit.test('Should change map size by given options', function(assert) {
   assert.ok($('#map-small-size').length, 'container element is not visible');
-  //assert.ok($('#map-small-size .gm-style').length, 'iframe is not visible');
+  assert.ok($('#map-small-size div').length, 'map is not visible');
   assert.equal($('#map-small-size').css('height'), '150px', 'height is not 150px');
   assert.equal($('#map-small-size').css('width'), '250px', 'width is not 250px');
+});
+
+QUnit.test('Should display map contols', function(assert) {
+  assert.ok($('#map-controls').length, 'container element is not visible');
+  assert.ok($('#map-controls div').length, 'map is not visible');
+
+  // street view
+  assert.ok($('#map-controls div[aria-label="Street View Pegman Control"]'.length), 'street view is not visible');
+
+  // pan
+  assert.ok($('#map-controls div[title="Pan up"]'.length), 'pan up is not visible');
+  assert.ok($('#map-controls div[title="Pan down"]'.length), 'pan down is not visible');
+  assert.ok($('#map-controls div[title="Pan right"]'.length), 'pan right is not visible');
+  assert.ok($('#map-controls div[title="Pan left"]'.length), 'pan left is not visible');
+
+  // zoom
+  assert.ok($('#map-controls div[title="Zoom in"]'.length), 'zoom in is not visible');
+  assert.ok($('#map-controls div[title="Zoom out"]'.length), 'zoom out is not visible');
 });
